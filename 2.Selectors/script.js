5f6889d0b748b267d10988327d6a7889fad65b2f
Hugo0O3 <hugomancini2.0@gmail.com>
2025-01-29 12:18:47 +0100

    ex 2

--- 2.Selectors/script.js
+++ 2.Selectors/script.js
@@ -1,2 +1,29 @@
-/*
-*/
+const important = document.querySelectorAll('.important')
+
+important.forEach((element => {
+    element.setAttribute("title", "This is an important item")
+}))
+
+const tags = document.querySelectorAll('img')
+
+tags.forEach((element) => {
+    if (!element.classList.contains('important')) {
+        element.style.display = "none"
+    }
+});
+
+const para = document.querySelectorAll('p')
+const randomColor = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+
+para.forEach((element) => {
+    console.log(element.textContent)
+    if (element.className) {
+        console.log(element.className)
+    } else {
+        const r = randomColor(0, 255)
+        const g = randomColor(0, 255)
+        const b = randomColor(0, 255)
+        const rgbColor = `rgb(${r}, ${g}, ${b})`
+        element.style.color = rgbColor
+    }
+});
