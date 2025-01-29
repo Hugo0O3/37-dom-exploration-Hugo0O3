6f1b3969c293f5a1692b1f47302daad26f70c699
Hugo0O3 <hugomancini2.0@gmail.com>
2025-01-29 14:59:56 +0100

    ex 3 fin mais bordel

--- 3.Create/script.js
+++ 3.Create/script.js
@@ -1,2 +1,46 @@
-/*
-*/
+const learners = ["test", "prout", "noob"]
+
+const randomColor = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+
+const article = document.querySelector('article')
+
+function findBrightness(r, g, b) {
+    return 0.2126 * r + 0.7152 * g + 0.0722 * b
+}
+
+const sections = []
+
+learners.forEach(learner => {
+    const r = randomColor(0, 255)
+    const g = randomColor(0, 255)
+    const b = randomColor(0, 255)
+    const rgbColor = `rgb(${r}, ${g}, ${b})`
+
+    const brightness = findBrightness(r, g, b)
+    
+    const section = document.createElement('section')
+    const para = document.createElement('p')
+    para.textContent = learner
+
+    section.style.backgroundColor = rgbColor
+
+    para.style.color = brightness < 128 ? 'white' : 'black'
+
+    section.appendChild(para)
+
+    sections.push(section)
+});
+
+sections.sort(() => Math.random() - 0.5)
+
+const h2 = article.querySelector('h2')
+
+const paraSort = Array.from(article.querySelectorAll('p'))
+paraSort.sort(() => Math.random() - 0.5)
+
+article.innerHTML = ""
+article.appendChild(h2)
+
+paraSort.forEach((p) => article.appendChild(p))
+
+sections.forEach(section => article.appendChild(section))
