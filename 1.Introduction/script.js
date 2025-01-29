11b453fd3de7855f8a897d911b82e6e4533c1e90
Hugo0O3 <hugomancini2.0@gmail.com>
2025-01-29 10:46:54 +0100

    ex1

--- 1.Introduction/script.js
+++ 1.Introduction/script.js
@@ -1,2 +1,14 @@
-/*
-*/
+const title = document.querySelector('title').textContent = 'Modifying the DOM'
+console.log(document.title)
+
+const randomColor = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+const r = randomColor(0, 255)
+const g = randomColor(0, 255)
+const b = randomColor(0, 255)
+const rgbColor = `rgb(${r}, ${g}, ${b})`
+
+const body = document.querySelector('body').style.backgroundColor = rgbColor
+
+for(const children of document.body.children[1].children){
+    console.log(children)
+}
